fe88954fe4474dc608cd652921c634658eeb65e5
Thomas Germon <thomas.germon@insa-rouen.fr>
2017-12-20 16:32:38 +0100

    Like

--- Adopteuninge/src/model/Like.java
+++ Adopteuninge/src/model/Like.java
@@ -1,5 +1,29 @@
-public class Like{
-  public void Like(){
-    
+public class Like {
+
+  private int nbLikeRestant;
+  private List<Utilisateur> listePersonnesLike;
+  private List<Utilisateur> listePersonnesQuiMontLike;
+
+  public Like(int nbLikeRestant, List<Utilisateur> listePersonnesLike, List<Utilisateur> listePersonnesQuiMontLike) {
+    this.nbLikeRestant = nbLikeRestant;
+    this.listePersonnesLike = listePersonnesLike;
+    this.listePersonnesQuiMontLike = listePersonnesQuiMontLike;
+  }
+
+  public int getNbLikeRestant() {
+    return nbLikeRestant;
+  }
+
+  public void setNbLikeRestant(int nb) {
+    this.nbLikeRestant = nb;
+  }
+
+  public void addPersonnesLike(Utilisateur utilisateur) {
+    listePersonnesLike.add(utilisateur);
+    nbLikeRestant -= 1;
+  }
+
+  public void addPersonnesQuiMontLike(Utilisateur utilisateur) {
+    listePersonnesQuiMontLike.add(utilisateur);
   }
 }
