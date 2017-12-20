544e3672bd89133dd2d88b36f5ee1d2782acdafc
Hugo Marchelidon <hugo.marchelidon@insa-rouen.fr>
2017-12-20 16:49:27 +0100

    ajout classe ChatPrive

---
+++ Adopteuninge/src/model/ChatPrive.java
@@ -0,0 +1,26 @@
+package model;
+import java.lang.*;
+
+public class ChatPrive {
+private Messages messages;
+private int id;
+private String nom;
+
+public ChatPrive(int id, String nom){
+        this.id=id;
+        this.nom=nom;
+        this.messages=new Messages();
+}
+
+public Messages getMessages(){
+        return this.messages;
+}
+
+public int getId(){
+        return this.id;
+}
+
+public String getNom(){
+        return this.nom;
+}
+}
