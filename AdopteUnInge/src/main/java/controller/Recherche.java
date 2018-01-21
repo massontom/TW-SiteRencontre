8f233c82bc65dad95a874e2fedfd1d32d3460346
Thomas Boucher <thomas.boucher@insa-rouen.fr>
2018-01-22 00:54:09 +0100

    recherche

--- AdopteUnInge/src/main/java/controller/Recherche.java
+++ AdopteUnInge/src/main/java/controller/Recherche.java
@@ -15,30 +15,26 @@ public class Recherche extends ActionSupport implements SessionAware {
   private List<Utilisateur> userRecherche;
   private String departement, ville, age;
   private int ageInt;
-  private SessionMap<String, Object> session;
+  private SessionMap<String, Object> sessionmap;
 
   public String input() throws Exception {
     return INPUT;
   }
 
   public void setUtilisateurs() throws SQLException, Exception {
-    System.out.println("setUtilisateurs");
     this.allUsers = Database.getAllUsers();
   }
 
   public void setDepartement(String dpt) {
     this.departement = dpt;
-    System.out.println("dpt: "+this.departement);
   }
 
   public void setVille(String ville) {
     this.ville = ville;
-    System.out.println("ville: "+this.ville);
   }
 
   public void setAge(String age) {
     this.age = age;
-    System.out.println("age: "+this.age);
   }
 
   public List<Utilisateur> getUtilisateurs(){
@@ -58,33 +54,29 @@ public class Recherche extends ActionSupport implements SessionAware {
   }
 
   public String setUserRecherche() throws SQLException, Exception {
-    System.out.println("setUserRecherche");
+    Utilisateur myuser = (Utilisateur)this.sessionmap.get("user");
     setUtilisateurs();
     this.userRecherche = new ArrayList<>();
     System.out.println("nb total d'utilisateurs :"+allUsers.size());
     for(Utilisateur user : allUsers) {
-      if (!(this.age.equals(""))) {
-        ageInt=Integer.parseInt(this.age);
-          System.out.println("Age :"+ageInt + user.getAge());
-        if ((user.getAge()<=ageInt+5) || (user.getAge()>=ageInt-5)) {
-          if (!(this.ville.equals(""))) {
-            System.out.println("ville");
-            if (this.ville.toLowerCase().equals(user.getVille().toLowerCase())) {
+      if (user.getSexe()==myuser.getOrientation()){
+        if (!(this.age.equals(""))) {
+          ageInt=Integer.parseInt(this.age);
+          if ((user.getAge()<=ageInt+5) || (user.getAge()>=ageInt-5)) {
+            if (!(this.ville.equals(""))) {
+              if (this.ville.toLowerCase().equals(user.getVille().toLowerCase())) {
+                userRecherche.add(user);
+              }
+            } else if (!(this.departement.equals(""))) {
+              if (this.departement.equals(Integer.toString(user.getDepartement()))) {
+                userRecherche.add(user);
+              }
+            } else {
               userRecherche.add(user);
             }
-          } else if (!(this.departement.equals(""))) {
-            System.out.println("dpt");
-            if (this.departement.equals(Integer.toString(user.getDepartement()))) {
-              userRecherche.add(user);
-            }
-          } else {
-            System.out.println("juste age");
-            userRecherche.add(user);
           }
-        }
 
-      } else {
-          System.out.println("2");
+        } else {
           if (!(this.ville.equals(""))) {
             if (this.ville.toLowerCase().equals(user.getVille().toLowerCase())) {
               userRecherche.add(user);
@@ -96,6 +88,7 @@ public class Recherche extends ActionSupport implements SessionAware {
           }
         }
       }
+    }
 
     return SUCCESS;
   }
@@ -106,7 +99,7 @@ public class Recherche extends ActionSupport implements SessionAware {
 
   @Override
   public void setSession(Map<String, Object> session) {
-    this.session=(SessionMap)session;
+    this.sessionmap=(SessionMap)session;
   }
 
 }
